21439432781ec4d5477fb3d0c7720dbcc488f9cd
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-06-03 23:13:38 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,9 @@ const {verify} = require("../controllers/auth");
 const routes = require("../controllers/user");
 
 router.get("/", verify ,routes.getAllUsers);
-router.get("/user/:id", routes.getUserById);
-router.get("/email/:email", routes.getUserByEmail);
-router.get("/count", routes.userCount);
-router.put("/update", routes.updateUser);
+router.get("/user/:id", verify ,routes.getUserById);
+router.get("/email/:email", verify , routes.getUserByEmail);
+router.get("/count", verify , routes.userCount);
+router.put("/update", verify , routes.updateUser);
 
 module.exports = router;
